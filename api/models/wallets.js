832b2a7030ba39922fcb43e740ee1eef5faa2e0e
Fernando Calixto <fernando.junior2906@gmail.com>
2022-05-14 13:41:50 -0300

    libs installation and environment preparation

---
+++ api/models/wallets.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class wallets extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  wallets.init({
+    name: DataTypes.STRING,
+    cpf: DataTypes.STRING,
+    birthdate: DataTypes.DATE,
+    empty: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'wallets',
+  });
+  return wallets;
+};
